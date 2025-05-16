ee3e672f7332a65dfb1022f10b856235cbf994ae
Michał <michal.t.partyka@gmail.com>
2025-05-16 19:27:06 +0200

    Initial commit

---
+++ src/main/java/com/task/collectionboxesforfundrasingevents/CollectionBoxesForFundrasingEventsApplication.java
@@ -0,0 +1,13 @@
+package com.task.collectionboxesforfundrasingevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionBoxesForFundrasingEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionBoxesForFundrasingEventsApplication.class, args);
+    }
+
+}
